ead167e9733e5d218159b381edba1d5ce6e8dd6b
tw1x <tw1xan@yandex.ru>
2023-08-07 18:54:34 +0300

    немного проработал оптимизацию, удалил копированный код и импортировал из другого js файла функцию которая приветствует и спрашивает имя пользователя

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
+import { cli } from './brain-games.js'
 
 console.log('Welcome to the Brain Games!')
-export const cli =() => {
-let userName = readlineSync.question('May I have your name? ');
-return console.log('Hello,' + userName + '!');
-};
-cli();
+
+
 
 
 
