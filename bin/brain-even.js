eba2967419fb355569dc45e9ff6f08e45e22c608
tw1x <tw1xan@yandex.ru>
2023-08-07 19:35:51 +0300

    небольшие правки brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
+
 import { cli } from './brain-games.js'
 
 console.log('Welcome to the Brain Games!')
 
-
-
-
-
 'Welcome to the Brain Games!'
+
