f5e3381d80d84d7a3fd59d32ec409fdb8e5c1ce0
tw1x <tw1xan@yandex.ru>
2023-08-08 11:58:33 +0300

    добавил предлагаемые условия игры

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,7 +4,48 @@ import readlineSync from 'readline-sync';
 
 import { cli } from './brain-games.js'
 
-console.log('Welcome to the Brain Games!')
 
-'Welcome to the Brain Games!'
 
+const getDescription = () => {
+    const description = console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    return description;
+};
+
+getDescription();
+//  export default () => {
+
+//     const countReplay = 3;
+
+    
+// };
+// export const randomNumber = (min, max) => {
+//     const randomMarker = Math.floor(Math.random() * (max - min + 1)) + min;
+//     return randomMarker;
+// };
+
+// export const evenNumber = (number) => number % 2 === 0;
+
+// export const getRandomAndQuestion = () => {
+//     const questions = randomNumber(1, 100);
+//     const rightAnswer = evenNumber(questions) ? 'yes' : 'no';
+//     return [questions, rightAnswer];
+// };
+// export const games = () => {
+//     getRandomAndQuestion(getDescription, getRandomAndQuestion);
+// };
+
+// for (let i = 0; i < roundsCount; i += 1) {
+//     const [question, correctAnswer] = getQuestionAndAnswer();
+//     console.log(`Question: ${question}`);
+//     const playerAnswer = readlineSync.question('You answer: ');
+
+//     if (correctAnswer === playerAnswer) {
+//       console.log('Correct!');
+//     } else {
+//       console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+//       console.log(`Let's try again, ${playerName}`);
+//       return;
+//     }
+//   }
+//   console.log(`Congratulations, ${playerName}`);
+// };
