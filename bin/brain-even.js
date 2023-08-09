7250253d82bea264df47c28da92e06641674dad2
tw1x <tw1xan@yandex.ru>
2023-08-09 21:51:17 +0300

    создал index.js, логика игр теперь будет там

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,38 +2,10 @@
 
 import readlineSync from 'readline-sync';
 
-
+import { playGame } from '../src/index.js'
 
 function isEven(number) {
   return number % 2 === 0;
-}
-
-function playGame() {
-  console.log("Welcome to the Brain Games!");
-  const name = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${name}!`);
-  console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-  let correctAnswers = 0;
-
-  while (correctAnswers < 3) {
-    const questionNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${questionNumber}`);
-
-    const userAnswer = readlineSync.question("Your answer: ");
-    const correctAnswer = isEven(questionNumber) ? "yes" : "no";
-
-    if (userAnswer === correctAnswer) {
-      console.log("Correct!");
-      correctAnswers++;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-}
+};
 
 playGame();
