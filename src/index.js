29d24b2ee068a5e55161ba6e24138c2496a992b0
tw1x <tw1xan@yandex.ru>
2023-08-15 13:29:23 +0300

    убрал помарки

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import readlineSync from 'readline-sync';
       };
 
     export function playGame() {
-      console.log("brain-calc\nWelcome to the Brain Games!");
+      console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${name}!\n`);
+  console.log(`Hello, ${name}!`);
     console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
   
     let correctAnswers = 0;
