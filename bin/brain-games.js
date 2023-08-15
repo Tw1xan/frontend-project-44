9d50f1412dcde4e8fa8d69ff36d03f574ed5ff84
tw1x <tw1xan@yandex.ru>
2023-08-15 13:16:20 +0300

    редактировал имя

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -6,8 +6,8 @@ import readlineSync from 'readline-sync';
 
 export const cli = () => {
 console.log('Welcome to the Brain Games!');
-let userName = readlineSync.question('May I have your name? ');
-return console.log('Hello,' + userName + '!');
+let name = readlineSync.question('May I have your name? ');
+return console.log('Hello,' + name + '!');
 };
 
 
