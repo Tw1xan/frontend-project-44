9868927637c8a7e0a1e3d56834fbb6cf38fc38ce
tw1x <tw1xan@yandex.ru>
2023-08-16 15:48:46 +0300

    раскидал не нужные файлы игры четность, сделал общую логику

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
-import { playGame} from '../src/index.js'
+import { cli, runGame, isEven } from '../src/index.js';
 
 
+const generateGameData = () => {
+  const number = Math.floor(Math.random() * 100) + 1;
+  const question = `${number}`;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
 
-  playGame();
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const evenGame = () => {
+  cli(description);
+  runGame(generateGameData);
+};
+evenGame();
+
+export default evenGame;
