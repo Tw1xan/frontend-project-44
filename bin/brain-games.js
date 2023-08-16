3beb7b141657b8eeec2c5a42864ea04e051a32be
tw1x <tw1xan@yandex.ru>
2023-08-16 14:24:18 +0300

    убрал помарки, добавил на приветствие cli()

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -6,10 +6,11 @@ import readlineSync from 'readline-sync';
 export const cli = () => {
 console.log('Welcome to the Brain Games!');
 let name = readlineSync.question('May I have your name? ');
-return console.log('Hello,' + name + '!');
+console.log('Hello,' + name + '!');
 };
 
-cli();
+
+
 
 
 
---
+++ bin/brain-progression.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { runGame } from '../src/index.js';
+import { cli } from './brain-games.js'
+
+
+const generateProgression = () => {
+  const progressionLength = Math.floor(Math.random() * 6) + 5; 
+  const hiddenIndex = Math.floor(Math.random() * progressionLength);
+  const startNumber = Math.floor(Math.random() * 100) + 1;
+  const step = Math.floor(Math.random() * 10) + 1;
+
+  const progression = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(startNumber + step * i);
+  }
+  const correctAnswer = progression[hiddenIndex].toString();
+  progression[hiddenIndex] = '..';
+
+  const question = progression.join(' ');
+  return [question, correctAnswer];
+};
+
+const description = 'What number is missing in the progression?';
+
+const progressionGameTop = () => {
+  cli(description);
+  runGame(generateProgression);
+};
+
+progressionGameTop();
+
+export default progressionGameTop;
