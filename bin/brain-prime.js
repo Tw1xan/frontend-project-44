bafc885aa179b72696a73d3f58e75c8b0b3a188e
tw1x <tw1xan@yandex.ru>
2023-08-16 14:39:09 +0300

    залил последнюю игру brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import { runGame } from '../src/index.js';
+import { cli } from './brain-games.js'
+
+
+const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+  if (num <= 3) {
+    return true;
+  }
+  if (num % 2 === 0 || num % 3 === 0) {
+    return false;
+  }
+  let i = 5;
+  while (i * i <= num) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+    i += 6;
+  }
+  return true;
+};
+
+const generateGameData = () => {
+  const number = Math.floor(Math.random() * 100) + 1;
+  const question = `${number}`;
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const brainPrime = () => {
+  welcome(description);
+  runGame(generateGameData);
+};
+
+export default brainPrime;
