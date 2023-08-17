ee89b6db390a668638cb855817cdaaeec5518ecf
tw1x <tw1xan@yandex.ru>
2023-08-17 09:59:09 +0300

    правки после проверки линтера в brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,19 +2,10 @@
 
 import readlineSync from 'readline-sync';
 
-
 export const cli = () => {
 console.log('Welcome to the Brain Games!');
-let name = readlineSync.question('May I have your name? ');
-console.log('Hello,' + name + '!');
-};
+  const name = readlineSync.question('May I have your name? ');
+  console.log('Hello,' + name + '!');
+  };
 
 cli();
-
-
-
-
-
-
-
-
