6f4c028778a144d88788f6950be8895519dbeecc
tw1x <tw1xan@yandex.ru>
2023-08-17 09:54:57 +0300

    редактировал brain-even после проверки линтера

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,7 +4,7 @@ import { cli, runGame } from '../src/index.js';
 
 function isEven(number) {
   return number % 2 === 0;
-};
+}
 
 const generateGameData = () => {
   const number = Math.floor(Math.random() * 100) + 1;
