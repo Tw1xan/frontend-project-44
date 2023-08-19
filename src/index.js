825657ae94c492812dae92daa58f6eca8ef0f1fd
tw1x <tw1xan@yandex.ru>
2023-08-19 23:50:50 +0300

    вынес константу до движка, и переименовал

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const roundsCount = 3;
+
 // общая логика игр и приветствие
 const runGame = (generateGameData, description) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!${description}`);
 
-  const numberOfQuestions = 3;
-
-  for (let i = 0; i < numberOfQuestions; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, correctAnswer] = generateGameData();
 
     console.log(`Question: ${question}`);
