770125f43e952a8e2906132923c24c3ef1faf602
tw1x <tw1xan@yandex.ru>
2023-09-12 10:52:19 +0300

    устранил помарки игры простых чисел, сделал по рекомендации наствника

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,40 +1,5 @@
 #!/usr/bin/env node
 
-import runGame from '../src/index.js';
-
-const isPrime = (num) => {
-  if (num <= 1) {
-    return false;
-  }
-  if (num <= 3) {
-    return true;
-  }
-  if (num % 2 === 0 || num % 3 === 0) {
-    return false;
-  }
-  let i = 5;
-  while (i * i <= num) {
-    if (num % i === 0 || num % (i + 2) === 0) {
-      return false;
-    }
-    i += 6;
-  }
-  return true;
-};
-
-const generateGameData = () => {
-  const number = Math.floor(Math.random() * 100) + 1;
-  const question = `${number}`;
-  const correctAnswer = isPrime(number) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
-
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-const brainPrime = () => {
-  runGame(generateGameData, description);
-};
+import brainPrime from '../src/games/prime.js';
 
 brainPrime();
-
-export default brainPrime;
