355a694f3e8d60cf83d9546e66416dca1a69fcdc
tw1x <tw1xan@yandex.ru>
2023-09-14 20:01:40 +0300

    добавление деления

--- src/games/calc.js
+++ src/games/calc.js
@@ -13,6 +13,11 @@ const calculation = (num1, num2, operation) => {
       return num1 - num2;
     case '*':
       return num1 * num2;
+    case '/':
+      if (num2 === 0) {
+        throw new Error("Division by zero is not allowed!");
+      }
+      return num1 / num2;
     default:
       throw new Error(`Operator ${operation} is incorrect!`);
   }
