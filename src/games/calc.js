66985c611e369e0038934ed931463bdc3da71920
tw1x <tw1xan@yandex.ru>
2023-09-14 19:45:06 +0300

    код отредактировал по замечаниям наставника

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,29 +1,31 @@
 import runGame from '../index.js';
 import getRandomInRange from '../utils.js';
 
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*', '/'];
+  return operators[Math.floor(Math.random() * operators.length)];
+};
+const calculation = (num1, num2, operation) => {
+  switch (operation) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Operator ${operation} is incorrect!`);
+  }
+};
+
 const generateGameData = () => {
   const num1 = getRandomInRange();
   const num2 = getRandomInRange();
-  const operators = ['+', '-', '*'];
-  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const operator = getRandomOperator();
   const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = calculation(num1, num2, operator);
 
-  let correctAnswer;
-  switch (operator) {
-    case '+':
-      correctAnswer = (num1 + num2).toString();
-      break;
-    case '-':
-      correctAnswer = (num1 - num2).toString();
-      break;
-    case '*':
-      correctAnswer = (num1 * num2).toString();
-      break;
-    default:
-      break;
-  }
-
-  return [question, correctAnswer];
+  return [question, correctAnswer.toString()];
 };
 
 const description = 'What is the result of the expression?';
--- src/games/progression.js
+++ src/games/progression.js
@@ -2,15 +2,22 @@ import runGame from '../index.js';
 import getRandomInRange from '../utils.js';
 
 const generateProgression = () => {
-  const progressionLength = Math.floor(Math.random() * 6) + 5;
-  const hiddenIndex = Math.floor(Math.random() * progressionLength);
+  const progressionLength = getRandomInRange(5, 10);
+  const hiddenIndex = getRandomInRange(0, progressionLength - 1);
   const startNumber = getRandomInRange();
-  const step = Math.floor(Math.random() * 10) + 1;
+  const step = getRandomInRange(1, 10);
 
-  const progression = [];
-  for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(startNumber + step * i);
-  }
+  const makeProgression = (start, stepi, length) => {
+    const progression = [];
+
+    for (let i = 0; i < length; i += 1) {
+      progression.push(start + stepi * i);
+    }
+
+    return progression;
+  };
+
+  const progression = makeProgression(startNumber, step, progressionLength);
   const correctAnswer = progression[hiddenIndex].toString();
   progression[hiddenIndex] = '..';
 
